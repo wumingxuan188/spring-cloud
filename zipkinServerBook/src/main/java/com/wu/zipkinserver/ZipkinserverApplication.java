64130a94572e37a0ba319bc75e98ad3e6ce54c2d
wumingxuan188 <wumingxuan188@126.com>
2019-06-30 14:42:36 +0800

    测试链路监控数据

---
+++ zipkinServerBook/src/main/java/com/wu/zipkinserver/ZipkinserverApplication.java
@@ -0,0 +1,18 @@
+package com.wu.zipkinserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin2.server.internal.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+@EnableEurekaClient
+
+public class ZipkinserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinserverApplication.class, args);
+    }
+
+}
